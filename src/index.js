1d6fe82a246f8b3a6dc141487c9f482ec3a7bc37
Felix Anducho <felix.anducho74@gmail.com>
2020-03-22 20:12:20 -0600

    Add changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const History = (props) => {
+  if (props.allClicks.lenght === 0){
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+    return (
+      <div>
+        buttton press history : {props.allClicks.join(' ')}
+      </div>
+    )
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {    
+    setAll(allClicks.concat('L'))    
+    setLeft(left + 1)  
+  }
+  const handleRightClick = () => {    
+    setAll(allClicks.concat('R'))    
+    setRight(right + 1)  
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text={'left'}/>
+        <Button onClick={handleRightClick} text={'right'}/>
+        {right}
+        <History allClicks={allClicks}/>
+        </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
